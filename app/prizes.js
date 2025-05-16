d71e9e0def3751bd9ab360f2374c9e806d74095b
Martin Woodward <martinwoodward@github.com>
2025-05-17 00:43:23 +0100

    Updates prizes for Build 2025

--- app/prizes.js
+++ app/prizes.js
@@ -1,14 +1,14 @@
 [
-  { "name": "an octocat", "quantity": 400, "path": "octocat.png" },
-  { "name": "a hat", "quantity": 500, "path": "hat.png" },
-  { "name": "a shirt", "quantity": 400, "path": "shirt1.png" },
-  { "name": "keycaps", "quantity": 1000, "path": "keycaps.png" },
-  { "name": "socks", "quantity": 500, "path": "socks.png" },
-  { "name": "a shirt", "quantity": 400, "path": "shirt2.png" },
-  { "name": "a belt bag", "quantity": 100, "path": "belt_bag.png" },
-  { "name": "a pin", "quantity": 2000, "path": "pin1.png" },
-  { "name": "a pin", "quantity": 2000, "path": "pin2.png" },
-  { "name": "hip pack", "quantity": 100, "path": "hip_pack.png" },
-  { "name": "a pennant", "quantity": 2000, "path": "pennant.png" },
-  { "name": "a shirt", "quantity": 400, "path": "shirt3.png" }
+  { "name": "a $100 coupon", "quantity": 11, "path": "coupon_100.png" },
+  { "name": "a book", "quantity": 24, "path": "book.png" },
+  { "name": "a $100 coupon", "quantity": 12, "path": "coupon_100.png" },
+  { "name": "a belt bag", "quantity": 10, "path": "belt_bag.png" },
+  { "name": "a $100 coupon", "quantity": 14, "path": "coupon_100.png" },
+  { "name": "hip pack", "quantity": 5, "path": "hip_pack.png" },
+  { "name": "a $100 coupon", "quantity": 14, "path": "coupon_100.png" },
+  { "name": "a figurine", "quantity": 5, "path": "mona.png" },
+  { "name": "a $100 coupon", "quantity": 14, "path": "coupon_100.png" },
+  { "name": "a hat", "quantity": 10, "path": "hat2.png" },
+  { "name": "a $100 coupon", "quantity": 14, "path": "coupon_100.png" },
+  { "name": "an LED sign", "quantity": 5, "path": "sign.png" }
 ]
