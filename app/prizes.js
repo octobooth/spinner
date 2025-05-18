0182b907998f6b5bed2945c918bac888d567e6a4
Martin Woodward <martinwoodward@github.com>
2025-05-18 14:28:58 -0700

    Add $50 coupon value graphic

--- app/prizes.js
+++ app/prizes.js
@@ -1,14 +1,14 @@
 [
-  { "name": "a shop coupon", "quantity": 10, "path": "coupon_100.png" },
+  { "name": "a shop coupon", "quantity": 10, "path": "coupon_50.png" },
   { "name": "a book", "quantity": 24, "path": "book.png" },
-  { "name": "a shop coupon", "quantity": 12, "path": "coupon_100.png" },
+  { "name": "a shop coupon", "quantity": 12, "path": "coupon_50.png" },
   { "name": "a belt bag", "quantity": 10, "path": "belt_bag.png" },
-  { "name": "a shop coupon", "quantity": 10, "path": "coupon_100.png" },
+  { "name": "a shop coupon", "quantity": 10, "path": "coupon_50.png" },
   { "name": "hip pack", "quantity": 5, "path": "hip_pack.png" },
-  { "name": "a shop coupon", "quantity": 10, "path": "coupon_100.png" },
+  { "name": "a shop coupon", "quantity": 10, "path": "coupon_50.png" },
   { "name": "a figurine", "quantity": 5, "path": "mona.png" },
-  { "name": "a shop coupon", "quantity": 10, "path": "coupon_100.png" },
+  { "name": "a shop coupon", "quantity": 10, "path": "coupon_50.png" },
   { "name": "a hat", "quantity": 10, "path": "hat2.png" },
-  { "name": "a shop coupon", "quantity": 10, "path": "coupon_100.png" },
+  { "name": "a shop coupon", "quantity": 10, "path": "coupon_50.png" },
   { "name": "an LED sign", "quantity": 5, "path": "sign.png" }
 ]
