f40f3a9525d0c981836a4c220dfa7e94663e3648
Martin Woodward <martinwoodward@github.com>
2025-05-19 21:37:59 -0700

    Update prizes list to include a signed book with increased quantity

--- app/prizes.js
+++ app/prizes.js
@@ -1,14 +1,14 @@
 [
-  { "name": "a shop coupon", "quantity": 10, "path": "coupon_50.png" },
-  { "name": "a book", "quantity": 24, "path": "book.png" },
-  { "name": "a shop coupon", "quantity": 12, "path": "coupon_50.png" },
-  { "name": "a belt bag", "quantity": 10, "path": "belt_bag.png" },
-  { "name": "a shop coupon", "quantity": 10, "path": "coupon_50.png" },
-  { "name": "hip pack", "quantity": 5, "path": "hip_pack.png" },
+  { "name": "a plushy", "quantity": 10, "path": "octocat.png" },
+  { "name": "a book", "quantity": 12, "path": "book.png" },
+  { "name": "some socks", "quantity": 20, "path": "socks.png" },
+  { "name": "a belt bag", "quantity": 3, "path": "belt_bag.png" },
+  { "name": "a pin", "quantity": 10, "path": "pin1.png" },
+  { "name": "a hip pack", "quantity": 2, "path": "hip_pack.png" },
   { "name": "a signed book", "quantity": 50, "path": "accelerate.png" },
   { "name": "a figurine", "quantity": 5, "path": "mona.png" },
+  { "name": "some keycaps", "quantity": 15, "path": "keycaps.png" },
+  { "name": "a hat", "quantity": 5, "path": "hat2.png" },
   { "name": "a shop coupon", "quantity": 10, "path": "coupon_50.png" },
-  { "name": "a hat", "quantity": 10, "path": "hat2.png" },
-  { "name": "a shop coupon", "quantity": 10, "path": "coupon_50.png" },
-  { "name": "an LED sign", "quantity": 5, "path": "sign.png" }
+  { "name": "an LED sign", "quantity": 3, "path": "sign.png" }
 ]
