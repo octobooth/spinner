de045aa58cba8967f969e77d1efe93b0767cea1c
Martin Woodward <martinwoodward@github.com>
2025-05-21 07:55:48 -0700

    Update prized

--- app/prizes.js
+++ app/prizes.js
@@ -1,14 +1,14 @@
 [
-  { "name": "a plushy", "quantity": 10, "path": "octocat.png" },
-  { "name": "a book", "quantity": 12, "path": "book.png" },
-  { "name": "some socks", "quantity": 20, "path": "socks.png" },
-  { "name": "a belt bag", "quantity": 3, "path": "belt_bag.png" },
-  { "name": "a pin", "quantity": 10, "path": "pin1.png" },
-  { "name": "a hip pack", "quantity": 2, "path": "hip_pack.png" },
-  { "name": "a signed book", "quantity": 50, "path": "accelerate.png" },
-  { "name": "a shop coupon", "quantity": 9, "path": "coupon_50.png" },
-  { "name": "some keycaps", "quantity": 15, "path": "keycaps.png" },
-  { "name": "a hat", "quantity": 5, "path": "hat2.png" },
-  { "name": "a shop coupon", "quantity": 9, "path": "coupon_50.png" },
-  { "name": "an LED sign", "quantity": 3, "path": "sign.png" }
+  { "name": "a plushy", "quantity": 12, "path": "octocat.png" },
+  { "name": "a signed book", "quantity": 7, "path": "accelerate.png" },
+  { "name": "a book", "quantity": 2, "path": "book.png" },
+  { "name": "some socks", "quantity": 15, "path": "socks.png" },
+  { "name": "a signed book", "quantity": 7, "path": "accelerate.png" },
+  { "name": "a pin", "quantity": 20, "path": "pin1.png" },
+  { "name": "a hip pack", "quantity": 1, "path": "hip_pack.png" },
+  { "name": "a signed book", "quantity": 8, "path": "accelerate.png" },
+  { "name": "some keycaps", "quantity": 10, "path": "keycaps.png" },
+  { "name": "a shop coupon", "quantity": 3, "path": "coupon_50.png" },
+  { "name": "a signed book", "quantity": 8, "path": "accelerate.png" },
+  { "name": "an LED sign", "quantity": 2, "path": "sign.png" }
 ]
