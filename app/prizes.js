fcd24123e915622e54560f06629e9880f7a8c22c
Martin Woodward <martinwoodward@github.com>
2025-07-29 14:53:30 +0100

    Refactor spinner to allow different numbers of prizes to be given away. Previously this was set to be 12 prize types

--- app/prizes.js
+++ app/prizes.js
@@ -1,14 +1,12 @@
-[
-  { "name": "a pin", "quantity": 50, "path": "pin1.png" },
+// Prize data for the spinner
+// Each prize has a name, quantity, and image path
+const PRIZE_DATA = [
+  { "name": "a pin", "quantity": 100, "path": "pin1.png" },
+  { "name": "a black hat", "quantity": 150, "path": "hat3.png" },
   { "name": "a tote bag", "quantity": 100, "path": "tote-i.png" },
-  { "name": "a can cooler", "quantity": 50, "path": "koozie.png" },
-  { "name": "a pin", "quantity": 50, "path": "pin1.png" },
-  { "name": "a black hat", "quantity": 50, "path": "hat3.png" },
-  { "name": "a pin", "quantity": 50, "path": "pin1.png" },
-  { "name": "a can cooler", "quantity": 50, "path": "koozie.png" },
-  { "name": "a black hat", "quantity": 50, "path": "hat3.png" },
-  { "name": "a pen", "quantity": 50, "path": "pen.png" },
-  { "name": "a can cooler", "quantity": 50, "path": "koozie.png" },
-  { "name": "a black hat", "quantity": 50, "path": "hat3.png" },
-  { "name": "a pen", "quantity": 100, "path": "pen.png" }
-]
+  { "name": "a pen", "quantity": 150, "path": "pen.png" },
+  { "name": "a can cooler", "quantity": 150, "path": "koozie.png" }
+];
+
+// Make it available globally for loading
+window.PRIZE_DATA = PRIZE_DATA;
