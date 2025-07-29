248dc9e70878d78e17b9a9fa41a74c591e3fc6b1
Martin Woodward <martinwoodward@github.com>
2025-07-29 14:15:04 +0100

    Add slight prize pun

--- app/prizes.js
+++ app/prizes.js
@@ -3,12 +3,12 @@
   { "name": "a tote bag", "quantity": 100, "path": "tote-i.png" },
   { "name": "a can cooler", "quantity": 50, "path": "koozie.png" },
   { "name": "a pin", "quantity": 50, "path": "pin1.png" },
-  { "name": "a hat", "quantity": 50, "path": "hat3.png" },
+  { "name": "a black hat", "quantity": 50, "path": "hat3.png" },
   { "name": "a pin", "quantity": 50, "path": "pin1.png" },
   { "name": "a can cooler", "quantity": 50, "path": "koozie.png" },
-  { "name": "a hat", "quantity": 50, "path": "hat3.png" },
+  { "name": "a black hat", "quantity": 50, "path": "hat3.png" },
   { "name": "a pen", "quantity": 50, "path": "pen.png" },
   { "name": "a can cooler", "quantity": 50, "path": "koozie.png" },
-  { "name": "a hat", "quantity": 50, "path": "hat3.png" },
+  { "name": "a black hat", "quantity": 50, "path": "hat3.png" },
   { "name": "a pen", "quantity": 100, "path": "pen.png" }
 ]
