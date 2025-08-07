e1d22e1e3a171d1e32d30f17ca617f044b0d871d
Laura Thorson <4021812+shmeadyy@users.noreply.github.com>
2025-08-07 13:47:46 -0700

    update BlackHat swag

--- app/prizes.js
+++ app/prizes.js
@@ -1,9 +1,9 @@
 // Prize data for the spinner
 // Each prize has a name, quantity, and image path
 const PRIZE_DATA = [
-  { "name": "a pin", "quantity": 100, "path": "pin1.png" },
-  { "name": "a black hat", "quantity": 150, "path": "hat3.png" },
-  { "name": "a tote bag", "quantity": 100, "path": "tote-i.png" },
+  { "name": "a pin", "quantity": 100, "path": "pin2.png" },
+  { "name": "a black hat", "quantity": 50, "path": "hat3.png" },
+  { "name": "a tote bag", "quantity": 50, "path": "tote-i.png" },
   { "name": "a pen", "quantity": 150, "path": "pen.png" },
   { "name": "a can cooler", "quantity": 150, "path": "koozie.png" }
 ];
